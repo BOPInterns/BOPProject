2a0d030a5e38de82c6fa4ec4355ccf5bce90baed
SeanRanieri <seanpranieri@gmail.com>
2023-05-18 15:14:15 +0800

    created register page

---
+++ bopfrontend/src/Components/Register.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+export const Register = () => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+        console.log(password);
+    }
+    
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <label for="firstName">First Name</label>
+                <input value={firstName} onChange={(e) => setFirstName(e.target.value)}type="firstName" placeholder="John" id="firstName" name="firstName"/>
+                <label for="lastname">Last Name</label>
+                <input value={lastName} onChange={(e) => setLastName(e.target.value)}type="lastName" placeholder="Smith" id="lastName" name="lastName"/>
+                <label for="email">Email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="email@domain.com" id="email" name = "email"/>
+                <br></br>
+                <label for="password">Password</label>
+                <input value={password} onChange={(e) => setPassword(e.target.value)}type="password" placeholder="********" id="password" name = "password"/>
+                <br></br>
+                <button type="submit">Register</button>
+            </form>
+            <button>Already have an account? Sign in here!</button>
+        </>
+    )
+}
