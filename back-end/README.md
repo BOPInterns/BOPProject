7d729b4d1daf3023c5e4b28421167cb76a68573c
Jonathan Mesa <mesajonathan@ufl.edu>
2023-05-25 09:33:52 +0800

    created first schema

--- back-end/README.md
+++ back-end/README.md
@@ -3,3 +3,8 @@
 2. do "npm install". This will isntall all required dependencies 
 3. to start backend do "npm run dev" to start in dev mode "npm run start" to start in deploy mode
 4. server now running on deployed port
+
+
+# Mongo DB
+Username: BoPInterns
+Password: Pratibimb123
--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,43 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
+
+const mongoURL = "mongodb+srv://BoPInterns:Pratibimb123@db1.l4iyumt.mongodb.net/";
+
+mongoose
+    .connect(mongoURL, { 
+        useNewUrlParser: true 
+    })
+    .then(() => {
+        console.log("successfully connected to mongo");
+    })
+    .catch((e) => console.log(e));
+
+
+app.listen(9000, () => { console.log("Server started on port 9000") });
+
+require("./schemas/userInfo");
+
+const User = mongoose.model("UserInfo");
+
+app.post("/register", async(req,res) => {
+    const {fname, lname, email, pword} = req.body;
+    
+    try{
+        await User.create({
+            firstName: fname,
+            lastName: lname,
+            userEmail: email,
+            password: pword,
+        });
+        res.send({status: "OK"});
+    } catch (error){
+        res.send({status: "error with register"})
+    }
+})
+
 
 app.get("/api", (req, res) =>{
     res.json({ "users": ["userOne", "userTwo", "userThree"] });
 });
 
-app.listen(9000, () => { console.log("Server started on port 9000") });
