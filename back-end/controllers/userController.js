87636ac010f121a67636ff2fa0704fb337b15b11
rachyoung <youngrachel@ufl.edu>
2023-05-29 12:10:00 +0800

    adding MVC files, ignore for now

---
+++ back-end/controllers/userController.js
@@ -0,0 +1,2 @@
+// Rachel testing stuff out, feel free to ignore
+const User = require("./../models/userModel.js");
---
+++ back-end/routes/userRoutes.js
@@ -0,0 +1,4 @@
+// Rachel testing stuff out, feel free to ignore
+const express = require("express");
+const userController = require("./../controllers/userController");
+const router = express.Router();
