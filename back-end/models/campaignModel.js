c98308433a10cd13f044cae8295c2f9e495107cc
rachyoung <youngrachel@ufl.edu>
2023-05-30 12:12:31 +0800

    campaign schema validators and more MVC stuff

--- back-end/models/campaignModel.js
+++ back-end/models/campaignModel.js
@@ -1,24 +1,69 @@
 const mongoose = require("mongoose");
 
 const campaignSchema = new mongoose.Schema({
-    name: String,
     owner: String,
-    tags: [String],
-    description: String,
-    challenge: String,
-    reach: String,
-    location: String,
-    goals: [String],
+    status: {
+        type: String,
+        enum: ["Challenge"],
+        default: "Challenge"
+    },
+    numActors: {
+        type: Number,
+        default: 0
+    },
+    // TODO: where will they enter the due date?
+    //       I missed it in the Figma prototype
+    deadline: Date,
+    caseStudy: {
+        type: Boolean,
+        default: false
+    },
+    // TODO: manual references to solution documents
+    solutions: [String],
+    //*** STEP 1
+    name: {
+        type: String,
+        required: [true, "Please provide a name for your campaign."],
+        maxlength: 100
+    },
+    // TODO: owner = reference to organization's _id
+    //       type = ObjectID
+    tags: {
+        type: [String],
+        required: [true, "Must choose at least 1 tag"]
+    },
+    video: String, // YT link
+    //*** STEP 2
+    description: {
+        type: String,
+        required: [true, "Please provide a description."]
+    },
+    challenge: {
+        type: String,
+        required: [true, "Please provide a challenge."]
+    },
+    mission: String,
     milestones: [String],
-    // status: enum options,
-    numActors: Number,
+    //*** STEP 3
+    location: {
+        type: String,
+        required: [true, "Please provide your location."]
+    },
+    reach: {
+        type: String,
+        required: [true, "Please provide your reach."]
+    },
     stakeholderLangs: [String],
     volunteerLangs: [String],
-    video: String, // YT link
+    //*** STEP 4
+    // TODO: use manual document references for this?
+    //       make a separate model to store all files
     otherFiles: [String],
-    deadline: Date,
-    caseStudy: Boolean,
-    solutions: [String]
+    //*** STEP 5
+    goals: {
+        type: String,
+        required: [true, "Please tell us your goals."]
+    }
 })
 
 const Campaign = mongoose.model("Campaign", campaignSchema);
--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -4,7 +4,7 @@ const userController = require("./../controllers/userController");
 const router = express.Router();
 
 router
-    .route("/")
+    .route("/register")
     .post(userController.createUser);
 
 router
