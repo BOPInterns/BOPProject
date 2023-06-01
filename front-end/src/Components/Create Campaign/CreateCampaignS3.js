e47dd7bf4b738640108a61da793eefa2edefaaf0
SeanRanieri <seanpranieri@gmail.com>
2023-06-01 12:55:47 +0800

    added map functionality on create campaign page

--- front-end/src/Components/Create Campaign/CreateCampaignS3.js
+++ front-end/src/Components/Create Campaign/CreateCampaignS3.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { NavigationBar } from '../NavigationBar';
 import { CreateCampaignNavbar } from './CreateCampaignNavbar';
 import Row from 'react-bootstrap/Row';
@@ -8,6 +9,21 @@ import Form from 'react-bootstrap/Form';
 import Image from'react-bootstrap/Image';
 
 export const CreateCampaignS3 = () => {
+    const [url, setUrl] = useState('https://www.openstreetmap.org/export/embed.html?bbox=-252.42187500000003%2C-64.16810689799152%2C191.25000000000003%2C85.80595815715571&amp;layer=mapnik;&amp;marker=-81%2c29%2c');
+
+    const handleChange = async () => {
+        const field = document.getElementById('location');
+        try {
+            let response = await fetch('https://geocode.maps.co/search?q=' + field.value);
+            let data = await response.json();
+            let lat = data[0].lat;
+            let lon = data[0].lon;
+            setUrl('https://www.openstreetmap.org/export/embed.html?bbox=' + (parseFloat(lon)-0.5) + '%2C' + (parseFloat(lat)-0.5) + '%2C' + (parseFloat(lon)+0.5) + '%2C' + (parseFloat(lat)+0.5) + '&amp;layer=mapnik&amp;marker=' + lon + '%2C' + lat);
+        } catch {
+            console.log('Error: could not find location.');
+        }
+    }
+
     return(
         <div>
             <NavigationBar />
@@ -34,11 +50,11 @@ export const CreateCampaignS3 = () => {
                             <br></br>
                             <FormGroup>
                                 <Form.Label>Reach</Form.Label>
-                                <Form.Control type="text" placeholder="eg.: India" />
+                                <Form.Control id="location" type="text" placeholder="eg.: India" onChange={handleChange}/>
                                 <Form.Text>
-                                    <Image src="holder.js/100px180">
-                                        
-                                    </Image>
+                                    <Card>
+                                        <iframe width="425" height="350" frameBorder="0" scrolling="no" marginHeight="2px" marginWidth="2px" src={url}></iframe><br/>
+                                    </Card>
                                 </Form.Text>
                                 <Form.Text className="text-muted">You can select the country or make the reach of the campaign using the map above</Form.Text>
                             </FormGroup>
