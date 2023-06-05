f4f3ed7ba4945408907e8a5af2fbddda89d456e5
Jonathan Mesa <mesajonathan@ufl.edu>
2023-06-05 12:35:14 +0800

    added file read functionality

---
+++ back-end/models/fileModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const fileSchema = new mongoose.Schema({
+    // owningCampaign: {
+    //     type: String,
+    // },
+    fileData: {
+        type: String,
+    }
+});
+
+const File = mongoose.model("File", fileSchema);
+
+module.exports = File;
