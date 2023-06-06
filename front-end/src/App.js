ab8a69b66841a7e71c474ab4ae8b6d8a21005e70
Jonathan Mesa <mesajonathan@ufl.edu>
2023-06-06 11:55:39 +0800

    added tags working properly from create campaign

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -18,7 +18,6 @@ import { KYCVerificationForm } from './Components/KYCVerification/KYCVerificatio
 import { KYCVerificationSubmit } from './Components/KYCVerification/KYCVerificationSubmit.js';
 import { RegisterSuccess  } from './Components/RegisterSuccess.js';
 import Private from './Components/Private.js';
-import { ResetPassword } from './Components/ResetPassword.js';    //only added for testing and might want to remove later for security differences
 
 const App = () => {
    const loginState = window.localStorage.getItem('loginState');
@@ -42,9 +41,7 @@ const App = () => {
            <Route path="/campaign-center" element={<CampaignCenter />} />
            <Route path="/Login" element={<Login />} />
            <Route path="/Register" element={<Register />} />
-           <Route path="/forgotpassword" element={<ForgotPassword />} />
-           <Route path="/get-reset-link" element={<ResetPassword />} />
-           <Route path="/resetpassword" element={<ResetPassword />} />
+           <Route path="/forgot-password" element={<ForgotPassword />} />
            <Route path="/create-campaign-introduction" element={<CreateCampaignS0 />} />
         </Routes>
      </>
