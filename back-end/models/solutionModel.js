1ac56fe51e618407f0978547799ab5a77504ce01
Jonathan Mesa <mesajonathan@ufl.edu>
2023-06-12 11:39:44 +0800

    added new schemas for solutions and services

---
+++ back-end/models/solutionModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const solutionSchema = new mongoose.Schema({
+    organization: String,
+
+    name: {
+        type: String,
+        required: [true, "Please provide a name for your solution."],
+        maxlength: 100
+    },
+    tags: {
+        type: [String],
+        required: [true, "Must choose at least 1 tag"]
+    },
+    numEndorsements: Number,
+    overview: {
+        type: String,
+        required: [true, "Please provide a solution overview."]
+    },
+    explanation: {
+        type: String,
+        required: [true, "Please provide a solution explanation."]
+    },
+    mission: String,
+    focusAreas: [String],
+    needs: [String],
+    technologies: [String],
+})
+
+const Solution = mongoose.model("Solution", solutionSchema);
+
+module.exports = Solution;
