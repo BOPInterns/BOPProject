98821f1b40b939c6bfce34549557ac21100625b8
brianguida <73550637+brianguida@users.noreply.github.com>
2023-06-14 11:58:56 +0800

    Dropdown button
    made dropdown in marketplace

---
+++ front-end/src/Components/MarketPlace/CampaignComp.js
@@ -0,0 +1,14 @@
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
+export const CampaignComp = () => {
+    
+    
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ front-end/src/Components/MarketPlace/SolutionComp.js
@@ -0,0 +1,7 @@
+export const SolutionComp = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
