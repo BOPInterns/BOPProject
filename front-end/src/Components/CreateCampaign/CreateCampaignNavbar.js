4714ce3cc625500af471a82fd7dd24e64ce88452
brianguida <73550637+brianguida@users.noreply.github.com>
2023-06-26 21:44:55 +0800

    Starting create campaign overhaul

--- front-end/src/Components/CreateCampaign/CreateCampaignNavbar.js
+++ front-end/src/Components/CreateCampaign/CreateCampaignNavbar.js
@@ -5,97 +5,95 @@ import Button from 'react-bootstrap/Button';
 import BottomNavbar from './BottomNavbar';
 import { BrowserRouter as Router, Switch, Route, useLocation } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
-import './customNavLink.css';
+import './CreateCampaignNavbar.css'
+import Pagination from 'react-bootstrap/Pagination';
+import { CreateCampaignS1 } from './CreateCampaignS1';
+import { CreateCampaignS2 } from './CreateCampaignS2';
+import { CreateCampaignS3 } from './CreateCampaignS3';
+import { CreateCampaignS4 } from './CreateCampaignS4';
+import { CreateCampaignS5 } from './CreateCampaignS5';
+import Container from 'react-bootstrap/Container';
 
 export const CreateCampaignNavbar = () => {
         
     const location = useLocation();
     const currentPage = Number(location.pathname.slice(-1));
+    
+    // const renderActivePage = () => {
+    //     switch (activePage) {
+    //       case 1:
+    //         return <div>
+    //             <CreateCampaignS1/>
+    //         </div>
+    //       case 2:
+    //         return <div>
+    //             <CreateCampaignS2/>
+    //         </div>;
+    //       case 3:
+    //         return <div>
+    //             <CreateCampaignS3/>
+    //         </div>
+    //       case 4:
+    //         return <div>
+    //             <CreateCampaignS4/>
+    //         </div>
+    //       case 5:
+    //         return <div>
+    //             <CreateCampaignS/>
+    //         </div>  
+    //       default:
+    //         return <div>
+    //             <CreateCampaignS1/>
+    //         </div>
+    //     }
+    //   };
         
     return (
         <div>
-            <Nav
-                justify variant="pills"
-                id="CreateCampaignTabs"
+        <Container
+            style={{
+                paddingTop: '40px',
+            }}
+        >
+            <Navbar
+                className="create-campaign-navbar"
             >
-                <Nav.Item>
-                    <Nav.Link 
-                    as={NavLink}
-                    exact
-                    to="/create-campaign-step-1"
-                    style={{
-                        color: 'rgb(145,148,149)',
-                        backgroundColor: 'rgb(33,37,41)',
-                        transition: 'background-color 0.3s ease',
-                        borderRadius: 0,    
-                    }}
-                    className="nav-link"
-                    activeClassname="active"
-                    href="/create-campaign-step-1">
+                <Navbar.Collapse
+                    className='create-campaign-navbar-collapse'
+                >
+                    <Nav.Link
+                        className='create-campaign-navbar-nav-link'
+                        href='/create-campaign-step-1'
+                    >
                         Step 1
                     </Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link 
-                    as={NavLink}
-                    exact
-                    to="/create-campaign-step-2"
-                    style={{
-                        color: '#fff',
-                        backgroundColor: 'rgb(33,37,41)',
-                        transition: 'background-color 0.3s ease',
-                        borderRadius: 0,
-                    }}
-                    href="/create-campaign-step-2">
+                    <Nav.Link
+                        className='create-campaign-navbar-nav-link'
+                        href='/create-campaign-step-2'
+                    >
                         Step 2
                     </Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link 
-                    as={NavLink}
-                    exact
-                    to="/create-campaign-step-3"
-                    style={{
-                        color: '#fff',
-                        backgroundColor: 'rgb(33,37,41)',
-                        transition: 'background-color 0.3s ease',
-                        borderRadius: 0,
-                    }}
-                    href="/create-campaign-step-3">
+                    <Nav.Link
+                        className='create-campaign-navbar-nav-link'
+                        href='/create-campaign-step-3'
+                    >
                         Step 3
                     </Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link 
-                    as={NavLink}
-                    exact
-                    to="/create-campaign-step-4"
-                    style={{
-                        color: '#fff',
-                        backgroundColor: 'rgb(33,37,41)',
-                        transition: 'background-color 0.3s ease',
-                        borderRadius: 0,
-                    }}
-                    href="/create-campaign-step-4">
-                        Step 4
-                    </Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link 
-                    as={NavLink}
-                    exact
-                    to="/create-campaign-step-5"
-                    style={{
-                        color: '#fff',
-                        backgroundColor: 'rgb(33,37,41)',
-                        transition: 'background-color 0.3s ease',
-                        borderRadius: 0,
-                    }}
-                    href="/create-campaign-step-5">
+                    <Nav.Link
+                        className='create-campaign-navbar-nav-link'
+                        href='/create-campaign-step-4'
+                    >
                         Step 5
                     </Nav.Link>
-                </Nav.Item>
-            </Nav>
+                    <Nav.Link
+                        className='create-campaign-navbar-nav-link'
+                        href='/create-campaign-step-5'
+                    >
+                        Step 5
+                    </Nav.Link>
+                </Navbar.Collapse>
+            </Navbar>
+        </Container>
             <BottomNavbar currentPage={currentPage}/>
         </div>
     )
